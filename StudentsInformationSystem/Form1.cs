4bb58b1820a7c9edc2587b997ceaf10765594b9d
npcnon <afkmhafric@gmail.com>
2024-02-16 18:23:18 +0800

    Add project files.

---
+++ StudentsInformationSystem/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StudentsInformationSystem
+{
+    public partial class FrmLogIn : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLogIn_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void labelControl3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
